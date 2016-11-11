fca1007e93314390591c684a3c2dbeaa2079baf2
burych <denis.burtsev@mail.ru>
2016-11-11 10:18:18 +0300

    2b

--- 2b/index.js
+++ 2b/index.js
@@ -5,19 +5,41 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  const query = url.parse(req.url, true).query;
-  const sum = String (query.fullname);
-  let name;
-  const substringArray = sum.split(" ");  
+  res.setHeader('access-control-allow-origin', 'http://account.skill-branch.ru');
+  res.setHeader('access-control-allow-methods', 'GET');
+  res.setHeader('access-control-allow-headers', 'content-type, accept');
+  res.setHeader('Content-Type', 'text/plain');
+  res.statusCode = 200;
 
+
+  function checkSymbols(name) {
+  return !((/\d|_|\//g).test(name));
+}
+  console.log(`--> ${req.url}`);
+
+  const query = url.parse(req.url, true).query;
+  let sum = String (query.fullname);
+  sum = sum.toLowerCase();
+  sum = sum.replace(/^ */g, "");
+  sum = sum.replace(/$ */g, "");
+  sum = sum.replace(/  */g, " ");
+  console.log(sum);
+
+  let name;
+  const substringArray = sum.split(" ");
   const l = substringArray.length;
+
+  for (i = 0; i < l; i ++)
+  {
+  	substringArray[i] = substringArray[i].charAt(0).toUpperCase() + substringArray[i].substr(1, substringArray[i].length-1);
+  }
+
+  if (String(substringArray[0].toString()) == "" || !checkSymbols(sum))
+  	res.end("Invalid fullname");
 
   switch(l){	
   case 1:
-  if (String(substringArray[0].toString()) != "")
   	name = String(substringArray[0].toString());
-  else
-  	name = String("Invalid fullname");
   break;
   case 2:
   name = String(substringArray[1].toString()) + " " + String(substringArray[0].toString()).charAt(0) + ".";
@@ -28,13 +50,7 @@ const server = http.createServer((req, res) => {
   default:
   name = String("Invalid fullname");
   }
-
-  res.setHeader('access-control-allow-origin', 'http://account.skill-branch.ru');
-  res.setHeader('access-control-allow-methods', 'GET');
-  res.setHeader('access-control-allow-headers', 'content-type, accept');
-  res.setHeader('Content-Type', 'text/plain');
-  res.statusCode = 200;
- 
+  console.log(`<-- ${name}`);
   res.end(name);
 });
 
